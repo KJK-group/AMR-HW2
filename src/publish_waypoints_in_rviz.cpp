f55cc8112922d0d6defb94fa4c25f1f40cbdafc7
Kristoffer Sørensen <kristoffer.pbs@gmail.com>
2022-04-07 08:39:00 +0200

    feat: add better interface for using tf2

--- src/publish_waypoints_in_rviz.cpp
+++ src/publish_waypoints_in_rviz.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
-#include <tf2/utils.h>
-#include <tf2_eigen/tf2_eigen.h>
-#include <tf2_ros/transform_listener.h>
+// #include <tf2/utils.h>
+// #include <tf2_eigen/tf2_eigen.h>
+// #include <tf2_ros/transform_listener.h>
 
 #include <eigen3/Eigen/Dense>
 #include <optional>
@@ -9,82 +9,73 @@
 #include <vector>
 
 #include "utils/rviz.hpp"
+#include "utils/transformlistener.hpp"
+
+#define let const auto
+#define var auto
+#define val const auto
+#define ref var&
+#define cref val&
+
+template <typename T>
+using Option = std::optional<T>;
+// using None = std::nullopt;
+
+// template <typename T>
+// using Some = std::make_optional<T>(T&&);
 
 using Eigen::Vector3f;
 using waypoints = std::vector<Vector3f>;
 
-auto lookup_tf(const std::string& to_frame, const std::string& from_frame,
-               tf2_ros::Buffer& tf_buffer, ros::Time time = ros::Time(0))
-    -> std::optional<geometry_msgs::TransformStamped> {
-    try {
-        return tf_buffer.lookupTransform(to_frame, from_frame, time);
-    } catch (tf2::TransformException& ex) {
-        ROS_WARN("%s", ex.what());
-        return std::nullopt;
-    }
-}
-
 auto main(int argc, char* argv[]) -> int {
     ros::init(argc, argv, "publish_waypoints_in_rviz");
-    auto nh = ros::NodeHandle();
-    auto pub_waypoints = nh.advertise<visualization_msgs::Marker>("/visualization_marker", 10);
-    auto loop_rate = ros::Rate(10);
-    auto publish = [&](const auto& msg) {
+    var nh = ros::NodeHandle();
+    var pub_waypoints = nh.advertise<visualization_msgs::Marker>("/visualization_marker", 10);
+    var loop_rate = ros::Rate(10);
+    var publish = [&](val& msg) {
         pub_waypoints.publish(msg);
         ros::spinOnce();
         loop_rate.sleep();
-        ;
     };
 
-    auto tf_buffer = tf2_ros::Buffer{};
-    auto tf_listener = tf2_ros::TransformListener(tf_buffer);
-    auto transform = geometry_msgs::TransformStamped{};
-    const auto to_frame = "map";
-    const auto from_frame = "inspection";
+    val loop_count = 10;
 
-    const auto origin = Vector3f{0, 0, 0};
-    const auto hover_waypoint = Vector3f{0.f, 0.f, 2.f};
+    var tf_listener = utils::transform::TransformListener{};
+    val to_frame = "map";
+    val from_frame = "inspection";
 
-    const auto wps = waypoints{{-2, -2, 0}, {-2, 2, 0}, {2, 2, 0}, {2, -2, 0}, {-2, -2, 0}};
+    val origin = Vector3f{0, 0, 0};
+    val hover_waypoint = Vector3f{0.f, 0.f, 2.f};
+    val wps = waypoints{{-2, -2, 0}, {-2, 2, 0}, {2, 2, 0}, {2, -2, 0}, {-2, -2, 0}};
+    val& wp0 = wps[0];
 
-    auto arrow_msg_gen = utils::rviz::arrow_msg_gen{};
-
+    var arrow_msg_gen = utils::rviz::arrow_msg_gen{};
     arrow_msg_gen.header.frame_id = "map";
     arrow_msg_gen.scale.x = 0.05f;
     arrow_msg_gen.scale.y = 0.2f;
     arrow_msg_gen.scale.z = 0.5f;
-    for (size_t i = 0; i < 25; i++) {
-        const auto msg = arrow_msg_gen({origin, hover_waypoint});
+
+    for (size_t i = 0; i < loop_count; i++) {
+        val msg = arrow_msg_gen({origin, hover_waypoint});
         publish(msg);
     }
 
-    if (const auto transform = lookup_tf(to_frame, from_frame, tf_buffer)) {
-        auto point_from_frame = geometry_msgs::PointStamped();
-        const auto& wp0 = wps.at(0);
-        point_from_frame.point.x = wp0.x();
-        point_from_frame.point.y = wp0.y();
-        point_from_frame.point.z = wp0.z();
-
-        auto point_to_frame = geometry_msgs::PointStamped();
-        tf2::doTransform(point_from_frame, point_to_frame, *transform);
-        for (size_t i = 0; i < 25; i++) {
-            const auto& point = point_from_frame.point;
-            const auto x = point.x;
-            const auto y = point.y;
-            const auto z = point.z;
-            const auto msg = arrow_msg_gen({hover_waypoint, {x, y, z}});
+    if (val opt = tf_listener.transform_vec3(to_frame, from_frame, wp0)) {
+        val point = *opt;
+        for (size_t i = 0; i < loop_count; i++) {
+            val msg = arrow_msg_gen({hover_waypoint, point});
             publish(msg);
         }
     }
 
     arrow_msg_gen.header.frame_id = "inspection";
-    for (size_t i = 0; i < 25; i++) {
-        auto it = wps.begin();
+    for (size_t i = 0; i < loop_count; i++) {
+        var it = wps.begin();
         it = std::next(it);
         for (; it != wps.end(); ++it) {
-            const auto& wp1 = *std::prev(it);
-            const auto& wp2 = *(it);
-            const auto msg = arrow_msg_gen({wp1, wp2});
+            val& wp1 = *std::prev(it);
+            val& wp2 = *(it);
+            val msg = arrow_msg_gen({wp1, wp2});
             pub_waypoints.publish(msg);
             ros::spinOnce();
             loop_rate.sleep();
